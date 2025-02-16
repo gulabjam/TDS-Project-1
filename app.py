d26f529e5b602544ca5503e40231d0d173866fc0
gulabjam <bhavinbiju2020@gmail.com>
2025-02-16 22:18:10 +0530

    Added files

--- app.py
+++ app.py
@@ -911,4 +911,4 @@ async def test1():
 
 if __name__ == '__main__':
     import uvicorn
-    uvicorn.run (app=app, host="127.0.0.1", port=8000)
+    uvicorn.run (app=app, host="localhost", port=8000)
